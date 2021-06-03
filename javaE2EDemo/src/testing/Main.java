109a249197a781e004258c4d52e32247ac7d1624
Vishakha <Vishakha@Vishakha-PC.mshome.net>
2021-06-03 11:33:19 +0530

    modified

--- javaE2EDemo/src/testing/Main.java
+++ javaE2EDemo/src/testing/Main.java
@@ -3,7 +3,7 @@ package testing;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("This is for Manifest test");
+		System.out.println("This is E2E test");
 
 	}
 
