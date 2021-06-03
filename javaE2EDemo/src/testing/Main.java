e364dda1be4dd6a4ea5ba2f2487a64be411e5d21
Vishakha <Vishakha@Vishakha-PC.mshome.net>
2021-06-03 18:37:59 +0530

    done

--- javaE2EDemo/src/testing/Main.java
+++ javaE2EDemo/src/testing/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hey...............This is E2E test");
+		System.out.println("Use Spring Boot Jar for Web Based Deployment");
 
 	}
 
